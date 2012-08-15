36b5ba5a4f71e9fbe4b2248eee58d4e6c7c71ddd
Rodrigo Dlugokenski <rddweb@gmail.com>
2012-08-15 12:08:04 -0300

    Badge!

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,5 @@
-<span style="float:left;padding-top:2px" class="footer"><?=date('d/m/Y H:i:s P');?> <b><?=$breadcumb?></b></span>
+<span style="float:left;padding-top:10px" class="footer"><?=date('d/m/Y H:i:s P');?> <b><?=$breadcumb?></b></span>
 <span class="footer">PRAV - Informática UFRGS / Vivo</span>
-<img src="<?=url::base()?>images/footer/valid-css.gif" alt="Valid CSS3" />
-<img src="<?=url::base()?>images/footer/valid-xhtml10.gif" alt="Valid XHTML1.0 Transitional" />
+<a href="http://www.w3.org/html/logo/">
+    <img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-graphics-performance-semantics.png" width="115" height="32" alt="HTML5 Powered with CSS3 / Styling, Graphics, 3D &amp; Effects, Performance &amp; Integration, and Semantics" title="HTML5 Powered with CSS3 / Styling, Graphics, 3D &amp; Effects, Performance &amp; Integration, and Semantics">
+</a>
