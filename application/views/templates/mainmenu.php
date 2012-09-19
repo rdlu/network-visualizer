50d2bc6d7b5e07c3af4970e240fb29718e1c4d69
Rodrigo Dlugokenski <rddweb@gmail.com>
2012-09-19 12:21:56 -0300

    Logo retorna para pagina inicial. fixes #21

--- application/views/templates/mainmenu.php
+++ application/views/templates/mainmenu.php
@@ -1,10 +1,14 @@
 <!-- MainMenu Template View -->
 <ul id="mainMenu">
-    <li id="titleMainMenu"><img src="<?=url::base()?>images/icon.png" alt="Logo" style="vertical-align:text-top;margin:0 5px;">NetMetric MoM</li>
-<?php foreach($menus as $key1=>$level1): ?>
-    <li id="mainMenu.<?=$key1?>"><?php if(isset($level1['href'])): ?><a href="<?=url::site($level1['href'])?>"><?=$level1['title']?></a><?php else: ?>
-            <?=$level1['title']?>
+    <li id="titleMainMenu"><a href="<?=url::base()?>" style="color: #f5f5f5;"><img src="<?=url::base()?>images/icon.png"
+                                                                                   alt="Logo"
+                                                                                   style="vertical-align:text-top;margin:0 5px;">NetMetric
+        MoM</a></li>
+    <?php foreach ($menus as $key1 => $level1): ?>
+    <li id="mainMenu.<?=$key1?>"><?php if (isset($level1['href'])): ?><a href="<?=url::site($level1['href'])?>"
+                                                                         class="mainMenu"><?=$level1['title']?></a><?php else: ?>
+        <?= $level1['title'] ?>
         <?php endif ?></li>
-<?php endforeach?>
+    <?php endforeach?>
 </ul>
 <!--End MainMenu -->
